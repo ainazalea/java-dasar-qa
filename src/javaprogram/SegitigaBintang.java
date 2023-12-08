88e3be2722c0a4724890951226023d6df905e01e
ainazalea <ainazalea7@gmail.com>
2023-12-08 23:47:40 +0700

    Update Tugas 2 (QA)

--- src/javaprogram/SegitigaBintang.java
+++ src/javaprogram/SegitigaBintang.java
@@ -8,12 +8,12 @@ public class SegitigaBintang {
             for (int i = 1; i <= rows * 2; i++) {
                 if (i <= rows) {
                     for (int j = 1; j <= i; j++) {
-                        System.out.print("x ");
+                        System.out.print("* ");
                     }
                 }
                 else {
                     for (int j = i; j <= rows * 2; j++){
-                        System.out.print("x ");
+                        System.out.print("* ");
                     }
                 }
                 System.out.println();
